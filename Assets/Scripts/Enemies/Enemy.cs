81f96f08873ecd2a057b86df9132815dd8a9109a
Daniel Pereira <danielpp95@outlook.com>
2019-12-21 02:24:26 +0000

    Added enemy rotation

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -9,6 +9,8 @@
     {
         public float speed = 5f;
         public GameObject HitPoint;
+        public GameObject RotationPart;
+        public float RotationSpeed = 10f;
 
         private Vector3 target;
 
@@ -31,6 +33,7 @@
             {
                 this.GetNextTarget();
             }
+            LookAtTarget();
         }
 
         private void GetNextTarget()
@@ -47,13 +50,26 @@
             }
         }
 
+        private void LookAtTarget()
+        {
+            var targetDirection = this.target - this.transform.position;
+
+            var lookRotation = Quaternion.LookRotation(targetDirection);
+
+            var rotation = Quaternion.Lerp(
+                this.RotationPart.transform.rotation,
+                lookRotation,
+                Time.deltaTime * this.RotationSpeed).eulerAngles;
+
+            this.RotationPart.transform.rotation = Quaternion.Euler(0, rotation.y, 0);
+        }
+
         private void GetPath()
         {
             var tilemap = FindObjectOfType<TilemapController>().tileMap;
             this.path = new List<Vector2Int>();
             this.path.AddRange(tilemap.FollowingPath);
             this.path.Add(tilemap.EndPoint);
-
         }
 
         public void TakeDamage(float damage)
